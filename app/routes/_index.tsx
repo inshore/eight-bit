6a6f5c9c5a2ccf191211cc9fe3cbf9211e121558
Daniel Mullin <email@danielmullin.com>
2023-04-14 12:50:01 +0200

    env

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -19,6 +19,11 @@ export default function Index () {
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
             <div className="absolute inset-0">
+            <img
+                className="h-full w-full object-cover"
+                src=""
+                alt="8Bit band!"
+              />
               <div className="absolute inset-0 bg-[color:rgba(255,56,56,0.5)] mix-blend-multiply" />
             </div>
             <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
